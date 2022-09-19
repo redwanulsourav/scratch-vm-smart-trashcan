a92c3675ccf65b87d819adb469e29fb5915cc684
redwanulsourav <redwanul.sourav@gmail.com>
2022-09-18 23:42:43 -0400

    feat: added get requests to server

--- src/blocks/scratch3_trashcan.js
+++ src/blocks/scratch3_trashcan.js
@@ -1,3 +1,5 @@
+const axios = require('axios')
+
 class Scratch3TrashcanBlocks {
     constructor (runtime) {
         /**
@@ -22,18 +24,50 @@ class Scratch3TrashcanBlocks {
 
     trashcan_move_forward(args, utils) {
         console.log('Trashcan move forward');
+        axios
+            .get('http://0.0.0.0:8701/trashcan/move_forward')
+            .then(res => {
+                console.log(`statusCode: ${res.status}`);
+            })
+            .catch(error => {
+                console.log(error);
+            });
     }
 
     trashcan_move_backward(args, utils) {
         console.log('Trashcan move backward');
+        axios
+            .get('http://0.0.0.0:8701/trashcan/move_backward')
+            .then(res => {
+                console.log(`statusCode: ${res.status}`);
+            })
+            .catch(error => {
+                console.log(error);
+            });
     }
 
     trashcan_rotate_cw(args, utils) {
         console.log('Trashcan rotate clockwise');
+        axios
+            .get('http://0.0.0.0:8701/trashcan/rotate_cw')
+            .then(res => {
+                console.log(`statusCode: ${res.status}`);
+            })
+            .catch(error => {
+                console.log(error);
+            });
     }
 
     trashcan_rotate_ccw(args, utils) {
         console.log('Trashcan rotate counter clockwise');
+        axios
+            .get('http://0.0.0.0:8701/trashcan/rotate_ccw')
+            .then(res => {
+                console.log(`statusCode: ${res.status}`);
+            })
+            .catch(error => {
+                console.log(error);
+            });
     }
 }
 
